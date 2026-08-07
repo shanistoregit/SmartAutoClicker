76d28f4b2819d754ac09fe12dacdc02f9d772c9a
shanistoregit <shanistoregit1@gmail.com>
2026-08-07 23:11:51 +0500

    Create WorkflowCategory.kt

---
+++ app/src/main/java/com/smartautoclicker/app/automation/WorkflowCategory.kt
@@ -0,0 +1,13 @@
+package com.smartautoclicker.app.automation
+
+data class WorkflowCategory(
+
+    val id: String,
+
+    val name: String,
+
+    val description: String = "",
+
+    val icon: String = ""
+
+)
