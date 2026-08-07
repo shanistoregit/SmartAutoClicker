3d5ad8a871bf41e999c463de003a29ba5f3671ae
shanistoregit <shanistoregit1@gmail.com>
2026-08-07 23:18:33 +0500

    Add WorkflowTrigger enum for automation events

---
+++ app/src/main/java/com/smartautoclicker/app/automation/WorkflowTrigger.kt
@@ -0,0 +1,17 @@
+package com.smartautoclicker.app.automation
+
+enum class WorkflowTrigger {
+
+    MANUAL,
+
+    APP_OPENED,
+
+    SCREEN_CHANGED,
+
+    TIMER,
+
+    BOOT_COMPLETED,
+
+    CUSTOM
+
+}
