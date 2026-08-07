633e9c5ad736c436df1e0a0b8fdafe37043dfc53
shanistoregit <shanistoregit1@gmail.com>
2026-08-07 23:13:33 +0500

    Create WorkflowConstants.kt

---
+++ app/src/main/java/com/smartautoclicker/app/automation/WorkflowConstants.kt
@@ -0,0 +1,23 @@
+package com.smartautoclicker.app.automation
+
+object WorkflowConstants {
+
+    const val DEFAULT_TIMEOUT = 10000L
+
+    const val DEFAULT_RETRY_COUNT = 3
+
+    const val DEFAULT_RETRY_DELAY = 1000L
+
+    const val MAX_HISTORY_SIZE = 100
+
+    const val MAX_LOG_SIZE = 500
+
+    const val MAX_WORKFLOW_NAME_LENGTH = 100
+
+    const val MAX_STEP_COUNT = 1000
+
+    const val DEFAULT_GROUP = "Default"
+
+    const val DEFAULT_CATEGORY = "General"
+
+}
