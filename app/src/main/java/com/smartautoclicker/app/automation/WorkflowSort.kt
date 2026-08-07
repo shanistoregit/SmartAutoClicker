b66894ed6fe0969b331fd74ccab53491f7cc4a78
shanistoregit <shanistoregit1@gmail.com>
2026-08-07 23:09:42 +0500

    Create WorkflowSort.kt

---
+++ app/src/main/java/com/smartautoclicker/app/automation/WorkflowSort.kt
@@ -0,0 +1,17 @@
+package com.smartautoclicker.app.automation
+
+enum class WorkflowSort {
+
+    NAME_ASC,
+
+    NAME_DESC,
+
+    CREATED_ASC,
+
+    CREATED_DESC,
+
+    UPDATED_ASC,
+
+    UPDATED_DESC
+
+}
